adf5d29746d5453353f56df09090c4dba5c295a5
rps-laptop <developer.rps@gmail.com>
2012-12-29 09:33:23 +0530

    added the text - Blur Level: <level> - to the image - displays and saves it - set limits for blur_level variable

--- 02_Blur-Image/main.c
+++ 02_Blur-Image/main.c
@@ -15,9 +15,20 @@ int main(int argc, char* argv[])
 	// necessary variables..
 	int blur_level = 11;
 	char key='q';
+	char s_blur_level[20] ;
+	char img_text[]="blur level: ";
 
 	// image container...
 	IplImage* img;
+
+	// Font obj for setting text font...
+	CvFont font;
+
+	// initialize font object...
+	cvInitFont(&font, CV_FONT_HERSHEY_SIMPLEX,
+			1.0, 1.0, 0, 1, CV_AA);
+
+
 
 	// starts the window thread...
 	// is necessary to destroy window...
@@ -36,14 +47,47 @@ int main(int argc, char* argv[])
 	printf("Enter the level of blurring: ");
 	scanf("%d", &blur_level);
 
+	// set limits 0-120
+	if(blur_level<=0)
+		blur_level=1;
+
+	else if(blur_level>120)
+		blur_level=119;
+
+
+
+	// if its even - some errors arise
+	//  so increment by 1 if even...
+	if(blur_level%2==0)
+		blur_level++;
+
+
 	// smooth the image...
-	//  save to blurred.jpg...
 	cvSmooth(img, img, CV_GAUSSIAN, blur_level, 0, 0, 0);
-	cvSaveImage("blurred.jpg", img, 0);
+
+
+	// write blur_level integer to String
+	sprintf(s_blur_level, "%d", blur_level) ;
+
+	// Concat "Blur Level: " and s_blur_level(actual value)...
+	strcat(img_text,s_blur_level);
+
+	// write text to image
+		cvPutText(img, img_text, cvPoint(10, 50),
+				   &font, cvScalar(255, 255, 255, 0));
+
+
 
 	// display image in the "Blurred" window...
-	cvNamedWindow("Blurred", CV_WINDOW_AUTOSIZE);
-	cvShowImage("Blurred", img);
+		cvNamedWindow("Blurred", CV_WINDOW_AUTOSIZE);
+		cvShowImage("Blurred", img);
+
+
+
+	//  save to blurred.jpg...
+	cvSaveImage("blurred.jpg", img, 0);
+
+
 
 	// get input from user
 	key = cvWaitKey(0);
