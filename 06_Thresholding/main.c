70b51b73a06e59039f5bd2e89713c3ce1cdc7472
rps-laptop <developer.rps@gmail.com>
2012-12-30 07:48:05 +0530

    created a function for thresholding BGR images - now working on HSV...

--- 06_Thresholding/main.c
+++ 06_Thresholding/main.c
@@ -17,11 +17,49 @@ Modified by RPS Deepan- 29/12/2012
 #include <cv.h>		// Main OpenCV library.
 #include <highgui.h>	// OpenCV functions for files and graphical windows.
 
+IplImage* bgrThresh(IplImage** img)
+{
+
+	/*
+	 * initialize 3 grayscale images
+	 *  to store 3 channels of img
+	 */
+	IplImage* red=cvCreateImage(cvGetSize(*img),8,1);
+	IplImage* green=cvCreateImage(cvGetSize(*img),8,1);
+	IplImage* blue=cvCreateImage(cvGetSize(*img),8,1);
+
+	// split src into 3 channels
+	cvSplit(*img, blue, green, red, NULL);
+
+
+	/*
+		 * to threshold red region alone
+		 *  we perform the following operation
+		 *   result = red - (blue + green)
+		 */
+		cvAdd(blue, green, green,NULL);
+		cvSub(red, green, red,NULL);
+
+		/*
+		 * start actual thresholding
+		 *  a threshold value of 20 is given
+		 */
+		cvThreshold(red, red, 20, 255, CV_THRESH_BINARY);
+
+
+	return red;
+
+}// end of method bgrThreshold()...
+
 
 int main(int argc, char* argv[])
 {
 	// load an image from file
 	IplImage* src=cvLoadImage("061.png",CV_LOAD_IMAGE_UNCHANGED);
+	IplImage* bgrThreshOp=cvCreateImage(cvGetSize(src),8,1);
+
+	cvStartWindowThread();
+
 
 	// if img file not available
 	//  print error msg and exit
@@ -32,52 +70,17 @@ int main(int argc, char* argv[])
 	}
 
 
-	/*
-	 * initialize 3 grayscale images
-	 *  to store 3 channels of src
-	 */
-	IplImage* red=cvCreateImage(cvGetSize(src),8,1);
-	IplImage* green=cvCreateImage(cvGetSize(src),8,1);
-	IplImage* blue=cvCreateImage(cvGetSize(src),8,1);
 
-	// split src into 3 channels
-	cvSplit(src, blue, green, red, NULL);
-
-
-	cvNamedWindow("input",CV_WINDOW_NORMAL);
+	// display original input image
+	cvNamedWindow("input",CV_WINDOW_AUTOSIZE);
 	cvShowImage("input",src);
 
-	/*
+	cvWaitKey(0);
 
-	cvNamedWindow("blueW",CV_WINDOW_NORMAL);
-	cvShowImage("blueW",blue);
+	bgrThreshOp=bgrThresh(&src);
 
-
-	cvNamedWindow("greenW",CV_WINDOW_NORMAL);
-	cvShowImage("greenW",green);
-
-
-	cvNamedWindow("redW",CV_WINDOW_NORMAL);
-	cvShowImage("redW",red);
-
-	*/
-
-	/*
-	 * to threshold red region alone
-	 *  we perform the following operation
-	 *   result = red - (blue + green)
-	 */
-	cvAdd(blue, green, green,NULL);
-	cvSub(red, green, red,NULL);
-
-	/*
-	 * start actual thresholding
-	 *  a threshold value of 20 is given
-	 */
-	cvThreshold(red, red, 20, 255, CV_THRESH_BINARY);
-
-	cvNamedWindow("redW",CV_WINDOW_NORMAL);
-	cvShowImage("redW",red);
+	cvNamedWindow("redW",CV_WINDOW_AUTOSIZE);
+	cvShowImage("redW",bgrThreshOp);
 
 	// wait from key event
 	cvWaitKey(0);
