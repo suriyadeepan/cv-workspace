f65c007ccbeacba1cd658497da661b1c1f5d517e
RPS-F17 <developer.rps@gmail.com>
2013-01-09 09:52:11 +0530

    00_Scratch-Pad/

--- 00_Scratch-Pad/main.c
+++ 00_Scratch-Pad/main.c
@@ -48,113 +48,99 @@ IplImage* invertImg(IplImage** img)
 	return *img;
 }
 
+void captureImage(IplImage** img)
+{
+	system("rm 002.avi");
+	system(" ffmpeg -f video4linux2 -r 25 -s 640x480 -i /dev/video1 -y -t 4 002.avi");
 
+
+
+		CvCapture* capture = 0;
+
+		capture = cvCreateFileCapture("002.avi");
+
+		if(!capture)
+		{
+			return;
+		}
+
+
+
+
+
+		int i=0;
+		while(1)
+		{
+
+		*img = cvQueryFrame(capture);
+
+		if(!*img)
+			break;
+
+
+		i++;
+
+		if(i>10)
+			break;
+
+
+		cvWaitKey(25);
+
+
+
+		}
+
+
+
+		cvNamedWindow("Source",CV_WINDOW_AUTOSIZE);
+			cvShowImage("Source",*img);
+
+			cvWaitKey(0);
+
+
+		// release capture
+		cvReleaseCapture(&capture);
+		cvDestroyWindow("Source");
+
+
+}
 
 int main(int argc, char* argv[])
 {
+	/*
+	 * use of ffmpeg for recording video using webcam
+	 *  then iterate thro' frames and pick a frame
+	 *   - i => device node
+	 *   - y => overwrite file
+	 *   - f => v4l2
+	 *   - s => size
+	 *   - t => time of recording
+	 *
+	 */
 
-
-	// load an image from file
-	//	IplImage* src=cvLoadImage("00.jpg",CV_LOAD_IMAGE_UNCHANGED);
-	//	IplImage* img;
-
-
-	CvCapture* capture = cvCreateFileCapture("00.avi");
-
+	IplImage* imgFromCam = 0;
 
 	cvStartWindowThread();
 
-
-	IplImage* frame = 0;
-	IplImage* frame1=0;
-	IplImage* frame2=0;
-
-	cvNamedWindow("Org",CV_WINDOW_AUTOSIZE);
-	cvNamedWindow("Bright",CV_WINDOW_AUTOSIZE);
-	cvNamedWindow("Contrast",CV_WINDOW_AUTOSIZE);
-	
-	frame=cvQueryFrame(capture);
-	
-
-	while(1)
-	{
-	
-	frame=cvQueryFrame(capture);
-	
-
-	if(!frame)
-	{  // capture a frame
-	    printf("Could not grab a frame\n\7");
-	    exit(0);
-	}
-
-	frame1=cvCloneImage(frame);
-	frame2=cvCloneImage(frame);
-
-	cvShowImage("Org",frame);
-
-	frame1=addBrightness(&frame1);	
-	
-	cvShowImage( "Bright", frame1);
-
-	frame2=addContrast(&frame2);
-
-	cvShowImage("Contrast",frame2);
-
-        char c = cvWaitKey(33);
-        if( c == 27 || c=='q') break;
+	captureImage(&imgFromCam);
 
 
-
-	}
+	cvNamedWindow("Source1",CV_WINDOW_AUTOSIZE);
+	cvShowImage("Source1",imgFromCam);
 
 
 
 
 
-
-/*
-
-	// if img file not available
-	//  print error msg and exit
-	if(!src)
-	{
-		printf("\nCheck input image file location...\nImage file not found!\n");
-		exit(1);
-	}
-
-	// display original input image
-	cvNamedWindow("input",CV_WINDOW_AUTOSIZE);
-	cvShowImage("input",src);
 
 	// wait for key event
 	cvWaitKey(0);
 
-	cvDestroyWindow("input");
-
-	src=invertImg(&src);
-
-
-	// display inverted image
-	cvNamedWindow("Inverted",CV_WINDOW_AUTOSIZE);
-	cvShowImage("Inverted",src);
-*/
-
-/*
-
-	// wait for key event
-	cvWaitKey(0);
-*/
 
 
 	// clean up memory...
 	cvDestroyAllWindows();
-
-	cvReleaseImage(&frame);
-	cvReleaseImage(&frame1);
-	cvReleaseImage(&frame2);
-
-	cvReleaseCapture(&capture);
+	cvReleaseImage(&imgFromCam);
 
 
 	return 0;
