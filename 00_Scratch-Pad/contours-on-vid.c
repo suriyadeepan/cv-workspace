d2f9ebd54acf66d09c614dc5f64394f648e0ddd1
RPS Deepan <developer.rps@gmail.com>
2013-03-24 22:35:15 +0530

    added a file contours-on-vid.c to 00/ - finds contours in each frame of a vid - modified README

--- 00_Scratch-Pad/contours-on-vid.c
+++ 00_Scratch-Pad/contours-on-vid.c
@@ -1,9 +1,47 @@
-// std headers
 #include <stdio.h>
 
-// opencv headers
 #include <cv.h>
 #include <highgui.h>
+
+
+
+// the method contourDetect()
+//  detect all contours and
+//   draws them on to a copy of src
+//    and returns it
+void contourDetect(IplImage** img)
+{
+	int i=0;
+
+	// sequence for storing resulting contours
+	CvSeq* contours;
+
+	// sequence for temporarily storing points
+	//  in a contour
+	CvSeq* result;
+
+	// memory storage space for storing contours
+	// 0 -> default (64K)
+	CvMemStorage *storage = cvCreateMemStorage(0);
+
+
+
+	// grayscale form of source image "img"
+	//  for contour detection
+	  IplImage* imgGray = cvCreateImage(cvGetSize(*img), 8, 1);
+	  	  cvCvtColor(*img, imgGray, CV_BGR2GRAY);
+
+
+	// find all contours in temp and
+	//  store them in contours
+	 cvFindContours(imgGray,storage, &contours,sizeof(CvContour),CV_RETR_LIST,
+		        CV_CHAIN_APPROX_SIMPLE, cvPoint(5,5));
+
+	 cvDrawContours(*img,contours,cvScalar(255,0,0,0),cvScalar(0,255,0,0),2,1,8,cvPoint(0,0));
+
+
+} // end of method contourDetect()...
+
 
 
 // --- MAIN ---
@@ -62,6 +100,13 @@ int main(int argc, char* argv[])
 			break;
 		}
 
+
+		// find contours on image
+		contourDetect(&frame);
+
+
+
+		// display frame
 		cvShowImage("Source",frame);
 
 	}
