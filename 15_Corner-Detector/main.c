dd55a2421833097fe70635512bbb388b5e3aa1a2
RPS Deepan <developer.rps@gmail.com>
2013-05-29 21:41:32 +0530

    worked on harris corner detector - changed the input image - use of sketch for drawing the squares - seems to work perfectly - still need to find contours, area, centroid of contours

--- 15_Corner-Detector/main.c
+++ 15_Corner-Detector/main.c
@@ -39,18 +39,71 @@ IplImage* adapThresh(IplImage* img)
 int main(int argc, char* argv[])
 {
 	IplImage* src = cvLoadImage(argv[1],CV_LOAD_IMAGE_GRAYSCALE);
+	IplImage* threshImg = cvCreateImage(cvGetSize(src), 8, 1);
 	IplImage* harrisImg = cvCreateImage(cvGetSize(src), IPL_DEPTH_32F, 1);
 
-	cvNamedWindow("Source",CV_WINDOW_NORMAL);
-	cvShowImage("Source",src);
+	cvStartWindowThread();
 
-	cvCornerHarris(src,harrisImg,8,5,0.04);
+	//cvNamedWindow("Source",CV_WINDOW_NORMAL);
+	//cvShowImage("Source",src);
+
+	cvThreshold(src,threshImg,100,255,CV_THRESH_BINARY);
+
+	//cvNamedWindow("Thresholded",CV_WINDOW_NORMAL);
+	//cvShowImage("Thresholded",threshImg);
+
+	cvCornerHarris(threshImg,harrisImg,20,5,0.04);
+
+
 
 	// display op
+	/*
 	cvNamedWindow("Corners",CV_WINDOW_NORMAL);
 	cvShowImage("Corners",harrisImg);
+	*/
+
+
+
+	// Blur
+	// smooth the image...
+	//cvSmooth(harrisImg, harrisImg, CV_GAUSSIAN, 19, 0, 0, 0);
+
+	/*
+	// MORPH
+	// a temp buffer for morph
+	IplImage* temp = cvCreateImage(cvGetSize(harrisImg),harrisImg->depth,1);
+	IplImage* morphedHarrisImg = cvCreateImage(cvGetSize(harrisImg),harrisImg->depth,1);
+	*/
+
+	/*
+	// operating on the harris output image
+	cvMorphologyEx(harrisImg,morphedHarrisImg,temp,NULL,CV_MOP_ERODE,3);
+	cvMorphologyEx(morphedHarrisImg,morphedHarrisImg,temp,NULL,CV_MOP_CLOSE,7);
+
+	cvSmooth(morphedHarrisImg,morphedHarrisImg, CV_GAUSSIAN, 19, 0, 0, 0);
+
+	// display morphed image
+	cvNamedWindow("Morphed",CV_WINDOW_NORMAL);
+	cvShowImage("Morphed",morphedHarrisImg);
+
+	*/
+
+	IplImage* cannyImg = cvCreateImage(cvGetSize(harrisImg),8,1);
+
+	cvConvertScaleAbs(harrisImg,cannyImg,1,0);
+	cvCanny(cannyImg,cannyImg,8,8,3);
+
+
+	// display canny image
+	cvNamedWindow("canny",CV_WINDOW_NORMAL);
+	cvShowImage("canny",cannyImg);
+
+
+
 
 	cvWaitKey(0);
+
+	cvDestroyAllWindows();
 
 	cvReleaseImage(&src);
 	cvReleaseImage(&harrisImg);
--- app_01_Image-Analyser/main.c
+++ app_01_Image-Analyser/main.c
@@ -24,11 +24,47 @@ IplImage* hImg;
 IplImage* sImg;
 IplImage* vImg;
 
+// corner image
+IplImage* cornImg;
+
 // font
 CvFont someFont;
 
 // info obtained from image
-char message[50];
+char message[70];
+
+// find corners
+void findCorners(IplImage* cornerIp)
+{
+
+	IplImage* threshImg = cvCreateImage(cvGetSize(cornerIp), 8, 1);
+	IplImage* harrisImg = cvCreateImage(cvGetSize(cornerIp), IPL_DEPTH_32F, 1);
+
+
+	cvThreshold(cornerIp,threshImg,100,255,CV_THRESH_BINARY);
+
+	cvCornerHarris(threshImg,harrisImg,20,5,0.04);
+
+	// Blur
+	// smooth the image...
+	cvSmooth(harrisImg, harrisImg, CV_GAUSSIAN, 19, 0, 0, 0);
+
+
+	// MORPH
+	// a temp buffer for morph
+	IplImage* temp = cvCreateImage(cvGetSize(harrisImg),harrisImg->depth,1);
+	IplImage* morphedHarrisImg = cvCreateImage(cvGetSize(harrisImg),harrisImg->depth,1);
+
+	// operating on the harris output image
+	cvMorphologyEx(harrisImg,morphedHarrisImg,temp,NULL,CV_MOP_ERODE,7);
+
+	// display morphed image
+	cvNamedWindow("Morphed",CV_WINDOW_NORMAL);
+	cvShowImage("Morphed",morphedHarrisImg);
+
+	cornImg = morphedHarrisImg;
+
+}
 
 // split src into RGB components
 void splitImagesRGB()
@@ -61,6 +97,14 @@ void mouseEvent(int evt, int x, int y, int flags, void* param){
 	// left mouse button
 	if(evt==CV_EVENT_LBUTTONDOWN){
 
+		// get a copy of src
+		IplImage* copyOfSrc = cvCloneImage(cornImg);
+
+
+
+
+
+		/*
 		// (x,y) => point where LMB is pressed
 		// obtain pixel values from RGB, HSV component @ point (x,y)
         int rVal = cvGetReal2D(rImg,y,x), gVal = cvGetReal2D(gImg,y,x), bVal = cvGetReal2D(bImg,y,x);
@@ -70,7 +114,11 @@ void mouseEvent(int evt, int x, int y, int flags, void* param){
         IplImage* copyOfSrc = cvCloneImage(src);
 
         // make a string from all the pixel values obtained
-        sprintf(message,"Position: %d %d ; RGB: %d %d %d ; HSV: %d %d %d",x,y,rVal,gVal,bVal,hVal,sVal,vVal);
+        sprintf(message,"Position: %d %d ; RGB: %d %d %d ; HSV: %d %d %d ; Mask: %d",
+        		x,y,rVal,gVal,bVal,hVal,sVal,vVal,applyMask(x,y));
+*/
+
+		sprintf(message,"Position: %d %d ; Mask: %d", x,y,applyMask(x,y));
 
         // draw the string "message" on the Image "copyOfSrc"
         cvPutText(copyOfSrc,message,cvPoint(50,copyOfSrc->height-100),&someFont,cvScalar(255,0,255,0));
@@ -81,11 +129,48 @@ void mouseEvent(int evt, int x, int y, int flags, void* param){
 
 }
 
-
-
-
-int main()
+int applyMask(int x,int y)
 {
+	/*
+	int width = src->width, height = src->height;
+
+	int val = 0,i=0,j=0;
+
+	if(x>2 && y>2 && x<width-1 && y<height-1)
+	{
+
+		for(i=x-1;i<=x+1;i++)
+		{
+			for(j=y-1;j<=y+1;j++)
+			{
+				if(i==x && j==y)
+					val += 4*cvGetReal2D(src,j,i);
+
+				else
+					val += 2*cvGetReal2D(src,j,i);
+			}
+		}
+/*
+	// get pixel value of each channel
+	int val = (2*cvGetReal2D(src, y-1, x-1))+(2*cvGetReal2D(src,y-1,x))+(2*cvGetReal2D(src,y-1,x+1))
+			+(2*cvGetReal2D(src,y,x-1))+(4*cvGetReal2D(src,y,x))+(2*cvGetReal2D(src,y,x+1))
+			+(2*cvGetReal2D(src,y+1,x-1))+(2*cvGetReal2D(src,y+1,x))+(2*cvGetReal2D(src,y+1,x+1));
+
+	return cvGetReal2D(src,y,x);
+
+	}
+*/
+
+	return cvGetReal2D(src,y,x);
+}
+
+
+int main(int argc, char* argv[])
+{
+		src = cvLoadImage(argv[1],CV_LOAD_IMAGE_GRAYSCALE);
+
+		cornImg = cvCreateImage(cvGetSize(src), IPL_DEPTH_32F, 1);
+
 		// create main window
 		cvNamedWindow("MyWindow",CV_WINDOW_AUTOSIZE);
 		cvStartWindowThread();
@@ -97,16 +182,16 @@ int main()
         // assigning the callback function for mouse events
         cvSetMouseCallback("MyWindow", mouseEvent, 0);
 
-        // load and display an image ./01.jpg
-        src = cvLoadImage("01.jpg",CV_LOAD_IMAGE_UNCHANGED);
+        findCorners(src);
 
         // displays image
-        cvShowImage("MyWindow", src);
+        cvShowImage("MyWindow", cornImg);
         
+        /*
         // split source image into components
         splitImagesRGB(src);
         splitImagesHSV(src);
-
+*/
 
         // wait for key press
         cvWaitKey(0);
